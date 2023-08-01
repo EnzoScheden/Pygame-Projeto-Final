8a02abb0431448ab4364b0d319e2b5cac1745812
Hector <hector.f.resende@gmail.com>
2023-08-01 10:53:15 -0300

    pp

--- PFPYGAME.py
+++ PFPYGAME.py
@@ -30,7 +30,6 @@ assets['navio'] = pygame.transform.scale(assets['navio'], (navio_largura, navio_
 assets['bala'] = pygame.image.load('img/laserRed16.png').convert_alpha()
 explosion_anim = []
 for i in range(9):
-    # Os arquivos de animação são numerados de 00 a 08
     filename = 'assets/img/regularExplosion0{}.png'.format(i)
     img = pygame.image.load(filename).convert()
     img = pygame.transform.scale(img, (32, 32))
@@ -67,14 +66,11 @@ class navio(pygame.sprite.Sprite):
     def shoot(self):
         # Verifica se pode atirar
         now = pygame.time.get_ticks()
-        # Verifica quantos ticks se passaram desde o último tiro.
         elapsed_ticks = now - self.last_shot
 
         # Se já pode atirar novamente...
         if elapsed_ticks > self.shoot_ticks:
-            # Marca o tick da nova imagem.
             self.last_shot = now
-            # A nova bala vai ser criada logo acima e no centro horizontal da nave
             new_bullet = Bullet(self.assets, self.rect.top, self.rect.centerx)
             self.groups['all_sprites'].add(new_bullet)
             self.groups['all_bullets'].add(new_bullet)
@@ -83,7 +79,6 @@ class navio(pygame.sprite.Sprite):
     
 class barquinho(pygame.sprite.Sprite):
     def _init_(self, assets):
-        # Construtor da classe mãe (Sprite).
         pygame.sprite.Sprite._init_(self)
 
         self.image = assets['barquinho']
@@ -98,8 +93,7 @@ class barquinho(pygame.sprite.Sprite):
         # Atualizando a posição do meteoro
         self.rect.x += self.speedx
         self.rect.y += self.speedy
-        # Se o meteoro passar do final da tela, volta para cima e sorteia
-        # novas posições e velocidades
+       
         if self.rect.top > altura or self.rect.right < 0 or self.rect.left > largura:
             self.rect.x = random.randint(0, largura-largura_barquinhos)
             self.rect.y = random.randint(-100, -altura_barquinhos)
@@ -111,14 +105,12 @@ class barquinho(pygame.sprite.Sprite):
 class Bullet(pygame.sprite.Sprite):
     # Construtor da classe.
     def _init_(self, assets, bottom, centerx):
-        # Construtor da classe mãe (Sprite).
         pygame.sprite.Sprite._init_(self)
 
         self.image = assets['bullet_img']
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
 
-        # Coloca no lugar inicial definido em x, y do constutor
         self.rect.centerx = centerx
         self.rect.bottom = bottom
         self.speedy = -10  # Velocidade fixa para cima
@@ -127,7 +119,6 @@ class Bullet(pygame.sprite.Sprite):
         # A bala só se move no eixo y
         self.rect.y += self.speedy
 
-        # Se o tiro passar do inicio da tela, morre.
         if self.rect.bottom < 0:
             self.kill()
 
@@ -140,43 +131,29 @@ class Explosion(pygame.sprite.Sprite):
         # Construtor da classe mãe (Sprite).
         pygame.sprite.Sprite._init_(self)
 
-        # Armazena a animação de explosão
         self.explosion_anim = assets['explosion_anim']
 
-        # Inicia o processo de animação colocando a primeira imagem na tela.
+        
         self.frame = 0  # Armazena o índice atual na animação
         self.image = self.explosion_anim[self.frame]  # Pega a primeira imagem
         self.rect = self.image.get_rect()
         self.rect.center = center  # Posiciona o centro da imagem
 
-        # Guarda o tick da primeira imagem, ou seja, o momento em que a imagem foi mostrada
         self.last_update = pygame.time.get_ticks()
-
-        # Controle de ticks de animação: troca de imagem a cada self.frame_ticks milissegundos.
-        # Quando pygame.time.get_ticks() - self.last_update > self.frame_ticks a
-        # próxima imagem da animação será mostrada
         self.frame_ticks = 50
 
     def update(self):
-        # Verifica o tick atual.
         now = pygame.time.get_ticks()
-        # Verifica quantos ticks se passaram desde a ultima mudança de frame.
         elapsed_ticks = now - self.last_update
 
-        # Se já está na hora de mudar de imagem...
         if elapsed_ticks > self.frame_ticks:
-            # Marca o tick da nova imagem.
             self.last_update = now
 
-            # Avança um quadro.
             self.frame += 1
 
-            # Verifica se já chegou no final da animação.
             if self.frame == len(self.explosion_anim):
-                # Se sim, tchau explosão!
                 self.kill()
             else:
-                # Se ainda não chegou ao fim da explosão, troca de imagem.
                 center = self.rect.center
                 self.image = self.explosion_anim[self.frame]
                 self.rect = self.image.get_rect()
@@ -185,7 +162,6 @@ class Explosion(pygame.sprite.Sprite):
 tempo = pygame.time.Clock()
 FPS = 30
 
-# Criando um grupo de meteoros
 all_sprites = pygame.sprite.Group()
 sprite_barquinhos = pygame.sprite.Group()
 sprite_projetil = pygame.sprite.Group()
@@ -197,7 +173,6 @@ groups['sprite_projetil'] = sprite_projetil
 # Criando o jogador
 player = navio(groups, assets)
 all_sprites.add(player)
-# Criando os meteoros
 for i in range(8):
     barco = barquinho(assets)
     all_sprites.add(barco)
@@ -249,28 +224,22 @@ while state != DONE:
     all_sprites.update()
 
     if state == PLAYING:
-        # Verifica se houve colisão entre tiro e meteoro
         hits = pygame.sprite.groupcollide(sprite_barquinhos, sprite_projetil, True, True, pygame.sprite.collide_mask)
-        for meteor in hits: # As chaves são os elementos do primeiro grupo (meteoros) que colidiram com alguma bala
-            # O meteoro e destruido e precisa ser recriado
+        for meteor in hits: 
             assets['destroy_sound'].play()
             m = barco(assets)
             all_sprites.add(m)
             sprite_barquinhos.add(m)
 
-            # No lugar do meteoro antigo, adicionar uma explosão.
             explosao = Explosion(meteor.rect.center, assets)
             all_sprites.add(explosao)
 
-            # Ganhou pontos!
             score += 100
             if score % 1000 == 0:
                 lives += 1
 
-        # Verifica se houve colisão entre nave e meteoro
         hits = pygame.sprite.spritecollide(player, sprite_barquinhos, True, pygame.sprite.collide_mask)
         if len(hits) > 0:
-            # Toca o som da colisão
             assets['boom_sound'].play()
             player.kill()
             lives -= 1
@@ -290,19 +259,16 @@ while state != DONE:
                 player = navio(groups, assets)
                 all_sprites.add(player)
 
-    # ----- Gera saídas
     tamanho.fill((0, 0, 0))  # Preenche com a cor branca
     tamanho.blit(assets['background'], (0, 0))
     # Desenhando meteoros
     all_sprites.draw(tamanho)
 
-    # Desenhando o score
     text_surface = assets['score_font'].render("{:08d}".format(score), True, (255, 255, 0))
     text_rect = text_surface.get_rect()
     text_rect.midtop = (largura / 2,  10)
     tamanho.blit(text_surface, text_rect)
 
-    # Desenhando as vidas
     text_surface = assets['score_font'].render(chr(9829) * lives, True, (255, 0, 0))
     text_rect = text_surface.get_rect()
     text_rect.bottomleft = (10, altura - 10)
