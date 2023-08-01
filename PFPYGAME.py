1f2c704dd56afdfd5584fd32af13ab4bda0024a0
Hector <hector.f.resende@gmail.com>
2023-08-01 10:37:19 -0300

    loop principal

--- PFPYGAME.py
+++ PFPYGAME.py
@@ -213,3 +213,102 @@ score = 0
 lives = 3
 
 # ===== Loop principal =====
+
+pygame.mixer.music.play(loops=-1)
+while state != DONE:
+    clock.tick(FPS)
+
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            state = DONE
+        # Só verifica o teclado se está no estado de jogo
+        if state == PLAYING:
+            # Verifica se apertou alguma tecla.
+            if event.type == pygame.KEYDOWN:
+                # Dependendo da tecla, altera a velocidade.
+                keys_down[event.key] = True
+                if event.key == pygame.K_LEFT:
+                    player.speedx -= 8
+                if event.key == pygame.K_RIGHT:
+                    player.speedx += 8
+                if event.key == pygame.K_SPACE:
+                    player.shoot()
+            # Verifica se soltou alguma tecla.
+            if event.type == pygame.KEYUP:
+                # Dependendo da tecla, altera a velocidade.
+                if event.key in keys_down and keys_down[event.key]:
+                    if event.key == pygame.K_LEFT:
+                        player.speedx += 8
+                    if event.key == pygame.K_RIGHT:
+                        player.speedx -= 8
+
+    # ----- Atualiza estado do jogo
+    # Atualizando a posição dos meteoros
+    all_sprites.update()
+
+    if state == PLAYING:
+        # Verifica se houve colisão entre tiro e meteoro
+        hits = pygame.sprite.groupcollide(all_meteors, all_bullets, True, True, pygame.sprite.collide_mask)
+        for meteor in hits: # As chaves são os elementos do primeiro grupo (meteoros) que colidiram com alguma bala
+            # O meteoro e destruido e precisa ser recriado
+            assets['destroy_sound'].play()
+            m = Meteor(assets)
+            all_sprites.add(m)
+            all_meteors.add(m)
+
+            # No lugar do meteoro antigo, adicionar uma explosão.
+            explosao = Explosion(meteor.rect.center, assets)
+            all_sprites.add(explosao)
+
+            # Ganhou pontos!
+            score += 100
+            if score % 1000 == 0:
+                lives += 1
+
+        # Verifica se houve colisão entre nave e meteoro
+        hits = pygame.sprite.spritecollide(player, all_meteors, True, pygame.sprite.collide_mask)
+        if len(hits) > 0:
+            # Toca o som da colisão
+            assets['boom_sound'].play()
+            player.kill()
+            lives -= 1
+            explosao = Explosion(player.rect.center, assets)
+            all_sprites.add(explosao)
+            state = EXPLODING
+            keys_down = {}
+            explosion_tick = pygame.time.get_ticks()
+            explosion_duration = explosao.frame_ticks * len(explosao.explosion_anim) + 400
+    elif state == EXPLODING:
+        now = pygame.time.get_ticks()
+        if now - explosion_tick > explosion_duration:
+            if lives == 0:
+                state = DONE
+            else:
+                state = PLAYING
+                player = Ship(groups, assets)
+                all_sprites.add(player)
+
+    # ----- Gera saídas
+    window.fill((0, 0, 0))  # Preenche com a cor branca
+    window.blit(assets['background'], (0, 0))
+    # Desenhando meteoros
+    all_sprites.draw(window)
+
+    # Desenhando o score
+    text_surface = assets['score_font'].render("{:08d}".format(score), True, (255, 255, 0))
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (WIDTH / 2,  10)
+    window.blit(text_surface, text_rect)
+
+    # Desenhando as vidas
+    text_surface = assets['score_font'].render(chr(9829) * lives, True, (255, 0, 0))
+    text_rect = text_surface.get_rect()
+    text_rect.bottomleft = (10, HEIGHT - 10)
+    window.blit(text_surface, text_rect)
+
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
