a11fa570ab159893369395d34664c6ad3dbb80e1
EnzoScheden <enzoss@al.insper.edu.br>
2023-08-01 08:59:15 -0300

    Iniciando variaveis

--- PFPYGAME.py
+++ PFPYGAME.py
@@ -1,1 +1,20 @@
-print(2)
+import pygame 
+
+import random 
+
+largura = 600
+
+altura = 480 
+
+tamanho = pygame.display.set_mode((largura,altura))
+
+
+
+largura_barquinhos = 50
+
+altura_barquinhos = 38 
+
+navio_largura = 50 
+
+navio_altura = 38
+
